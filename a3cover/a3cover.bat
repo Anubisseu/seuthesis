dc59afd687dbc84144fdaf0d6cfe8b3da1c49b95
王彬 <wangbiu@foxmail.com>
2015-11-20 10:21:10 +0800

    修改a3cover脚本，使用本地pdfcrop，并且修正a3cover.bat脚本中的删除命令
    Signed-off-by: 王彬 <wangbiu@foxmail.com>

--- a3cover/a3cover.bat
+++ a3cover/a3cover.bat
@@ -1,9 +1,9 @@
 pdflatex -interaction=nonstopmode bookspine_hor.tex
 pdflatex -interaction=nonstopmode bookspine_ver.tex
-pdfcrop --margins 2 --clip bookspine_ver.pdf bookspine_res.pdf
+./pdfcrop.pl --margins 2 --clip bookspine_ver.pdf bookspine_res.pdf
 pdflatex -interaction=nonstopmode A3cover.tex
 
-rm *.aux *.log
+del *.aux *.log
 
-rm bookspine_hor.pdf
-rm bookspine_ver.pdf
+del bookspine_hor.pdf
+del bookspine_ver.pdf
--- a3cover/a3cover.sh
+++ a3cover/a3cover.sh
@@ -2,7 +2,7 @@
 
 pdflatex -interaction=nonstopmode bookspine_hor.tex
 pdflatex -interaction=nonstopmode bookspine_ver.tex
-pdfcrop --margins 2 --clip bookspine_ver.pdf bookspine_res.pdf
+./pdfcrop.pl --margins 2 --clip bookspine_ver.pdf bookspine_res.pdf
 pdflatex -interaction=nonstopmode A3cover.tex
 
 rm *.aux *.log
